b7efaa7e8129238a6e3de7569526181de947c011
Donaldo Guevara <dagiguevara@gmail.com>
2017-10-03 13:11:36 -0600

    first commit

---
+++ src/test/java/acklen/selenium/tutorial/GoogleSearch.java
@@ -0,0 +1,19 @@
+package acklen.selenium.tutorial;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class GoogleSearch {
+  @Test
+  public void googleSearch() {
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+}
